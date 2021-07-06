535f14ec583fd307c91e241bd6a7834d5683511d
AnhThu <nguyenanhthu_8888@yahoo.com.vn>
2021-07-06 11:50:51 +0100

    Class File erstellt

---
+++ FileManagerCALM/BusinessLogic/File.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class File
+    {
+       public string Name { get; set; }
+       public FileTypeEnum Typ { get; set; }
+       public string Text { get; set; }
+       public string Pfad { get; set; }
+        StatusEnum Status;
+
+    }
+
+   
+}
--- FileManagerCALM/BusinessLogic/FileTypeEnum.cs
+++ FileManagerCALM/BusinessLogic/FileTypeEnum.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace BusinessLogic
 {
-    public class Class1
-    {
+  public  enum FileTypeEnum
+    { 
+       pdf, txt
     }
 }
