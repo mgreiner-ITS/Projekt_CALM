09e38247b9b052eb89bcedfdaa9341f18d8d7bb2
mgreiner-ITS <86648737+mgreiner-ITS@users.noreply.github.com>
2021-09-17 12:35:14 +0200

    added viewmodels ?

---
+++ FileManagerCALM/DataSelector/ViewModel/DirectoryItemViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace DataSelector.ViewModel
+{
+    class DirectoryItemViewModel : ViewModelBase
+    {
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _path;
+
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool _status;
+
+        public bool Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<DirectoryItemViewModel> SubDirectoryViewModels { get; set; } = new ObservableCollection<DirectoryItemViewModel>();
+        public ObservableCollection<FileItemViewModel> FileViewModels { get; set; } = new ObservableCollection<FileItemViewModel>();
+
+    }
+}
---
+++ FileManagerCALM/DataSelector/ViewModel/FileItemViewModel.cs
@@ -0,0 +1,107 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSelector.ViewModel
+{
+    class FileItemViewModel : ViewModelBase
+    {
+        private long _id;
+
+        public long Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private FileType _type;
+
+        public FileType Type
+        {
+            get { return _type; }
+            set
+            {
+                _type = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _content;
+
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _path;
+
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _partition;
+
+        public string Partition
+        {
+            get { return _partition; }
+            set
+            {
+                _partition = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime _lastModified;
+
+        public DateTime LastModified
+        {
+            get { return _lastModified; }
+            set
+            {
+                _lastModified = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool _status;
+
+        public bool Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
